528203f032f96492edd1f3e3b10cd980d565cc95
Matheus <mfkovaleski@inf.ufrgs.br>
2022-02-09 00:29:54 -0300

    exercicio 1

---
+++ Desenvolvimento Mobile 1/Exercicio da aula do dia 08_02/Main.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+
+    val pr_totais : Float = 50F
+    var faltas : Float = 10F
+    var n1 : Float = 8F
+    var n2 : Float = 10F
+
+    var faltas_por : Float = (pr_totais - faltas)/(pr_totais) * 100
+
+    println(faltas_por)
+
+    if(faltas_por >= 75)
+    {
+        var result : Float = (n1 + n2)/2
+
+        if(result >= 9)
+            println("A")
+        else if(result >= 7.5)
+            println("A")
+        else if(result >= 6)
+            println("A")
+        else
+            println("D")
+    }
+    else
+    {
+        println("FF")
+    }
+}
---
+++ Desenvolvimento Mobile 1/Exercicio da aula do dia 08_02/README.md
@@ -0,0 +1,15 @@
+# Desenvolvimento Mobile
+
+Exercicio:
+
+Faça um programa que, dado dois valores quaisquer que representação a nota de um aluno e outro valor que representa a quantidade de faltas, faça:
+
+calculo da media;
+mostre o GPA do aluno
+(
+M >= 6 -> C
+M >= 7.5 -> B
+M >= 9 -> A
+M <= 6 -> D
+faltas < 75% -> FF
+)
